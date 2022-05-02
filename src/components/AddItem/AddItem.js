b707605a6984d7b26677f414bb57f7d5d08f2897
HossainRafi <hossainrafi71@gmail.com>
2022-05-02 13:02:38 +0600

    design changes

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -15,59 +15,65 @@ const AddItem = () => {
   };
 
   return (
-    <form onSubmit={handleForm} className="w-1/2 mx-auto my-16 border-2 py-16">
-      <input
-        type="text"
-        name="name"
-        placeholder="Enter Product Name"
-        required
-        className="block border-2 m-2 w-10/12 mx-auto"
-      />
-      <input
-        type="email"
-        name="email"
-        required
-        className="block border-2 m-2 w-10/12 mx-auto"
-      />
-      <input
-        type="text"
-        name="url"
-        placeholder="Enter Image URL"
-        required
-        className="block border-2 m-2 w-10/12 mx-auto"
-      />
-      <textarea
-        name="desc"
-        placeholder="Write Description Here"
-        className="block border-2 m-2 w-10/12 mx-auto resize-none"
-      />
-      <input
-        type="text"
-        name="supplier"
-        placeholder="Enter Supplier Name"
-        required
-        className="block border-2 m-2 w-10/12 mx-auto"
-      />
-      <input
-        type="number"
-        name="quantity"
-        placeholder="Enter Product Quantity"
-        required
-        className="block border-2 m-2 w-10/12 mx-auto"
-      />
-      <input
-        type="number"
-        name="price"
-        placeholder="Enter Product Price"
-        required
-        className="block border-2 m-2 w-10/12 mx-auto"
-      />
-      <input
-        type="submit"
-        value="Add Item"
-        className="mx-auto bg-slate-500 px-3 py-1 flex justify-center mt-5 rounded-md text-xl font-bold"
-      />
-    </form>
+    <div className="bg-gray-200 py-10">
+      <form
+        onSubmit={handleForm}
+        className="rounded-2xl w-1/2 mx-auto py-5 bg-gray-400"
+          >
+              <h1 className="flex justify-center text-4xl font-bold font-serif text-blue-700 pt-5 pb-5">Fill This Form</h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Product Name"
+          required
+          className="block py-1 pl-3 rounded-md text-lg bg-gray-200 border-2 m-4 w-9/12 mx-auto"
+        />
+        <input
+          type="email"
+          name="email"
+          required
+          className="block py-1 pl-3 rounded-md text-lg bg-gray-200 border-2 m-4 w-9/12 mx-auto"
+        />
+        <input
+          type="text"
+          name="url"
+          placeholder="Enter Image URL"
+          required
+          className="block py-1 pl-3 rounded-md text-lg bg-gray-200 border-2 m-4 w-9/12 mx-auto"
+        />
+        <textarea
+          name="desc"
+          placeholder="Write Description Here"
+          className="block py-1 pl-3 rounded-md text-lg bg-gray-200 border-2 m-4 w-9/12 mx-auto resize-none"
+        />
+        <input
+          type="text"
+          name="supplier"
+          placeholder="Enter Supplier Name"
+          required
+          className="block py-1 pl-3 rounded-md text-lg bg-gray-200 border-2 m-4 w-9/12 mx-auto"
+        />
+        <input
+          type="number"
+          name="quantity"
+          placeholder="Enter Product Quantity"
+          required
+          className="block py-1 pl-3 rounded-md text-lg bg-gray-200 border-2 m-4 w-9/12 mx-auto"
+        />
+        <input
+          type="number"
+          name="price"
+          placeholder="Enter Product Price"
+          required
+          className="block py-1 pl-3 rounded-md text-lg bg-gray-200 border-2 m-4 w-9/12 mx-auto"
+        />
+        <input
+          type="submit"
+          value="Add Item"
+          className="mx-auto bg-slate-500 px-5 py-1 flex justify-center mt-8 rounded-md text-xl font-bold"
+        />
+      </form>
+    </div>
   );
 };
 
